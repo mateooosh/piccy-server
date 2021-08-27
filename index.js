7a13989d034da39cc388c2c433d65351024882a3
mateooosh <mateusz20111@op.pl>
2021-08-27 18:09:47 +0200

    added getting messages by channel id

--- index.js
+++ index.js
@@ -70,11 +70,11 @@ io.on("connection", function (socket) {
 
   //get channels by idUser
   app.get('/channels', (req, res) => {
-    console.log(req.query);
+    // console.log(req.query);
     if (req.query.idUser) {
       const query = `SELECT idUser, idChannel, username, name, photo, (SELECT message from messages m WHERE m.idChannel=uc.idChannel ORDER BY createdAt DESC LIMIT 1) as lastMessage, 
                     (SELECT createdAt from messages m WHERE m.idChannel=uc.idChannel ORDER BY createdAt DESC LIMIT 1) as createdAt FROM users JOIN users_channels uc ON users.id=uc.idUser 
-                    WHERE uc.idChannel IN (SELECT idChannel FROM users_channels WHERE idUser=${req.query.idUser}) AND uc.idUser!=${req.query.idUser}`;
+                    WHERE uc.idChannel IN (SELECT idChannel FROM users_channels WHERE idUser=${req.query.idUser}) AND uc.idUser!=${req.query.idUser} ORDER BY createdAt DESC`;
       connection.query(query, async (err, rows) => {
         if (err) throw err;
         for (let item of rows) {
@@ -82,6 +82,7 @@ io.on("connection", function (socket) {
             const image = await fun.resizeImage(item.photo, 60, 60);
             item.photo = fun.bufferToBase64(image);
           }
+          item.isActive = activeUsers.has(item.username);
         }
 
         res.json(rows);
@@ -91,12 +92,50 @@ io.on("connection", function (socket) {
     }
   })
 
+  //get messages by channel
+  app.get('/messages/:channel', (req, res) => {
+    // console.log(req.query);
+    const messagesQuery = `SELECT id, idSender, message, createdAt FROM messages WHERE idChannel=${req.params.channel}`;
+
+    const result = {};
+    let promise = new Promise((resolve, reject) => {
+      connection.query(messagesQuery, (err, rows) => {
+        if (err) throw err;
+        result.messages = rows;
+        resolve();
+      })
+    })
+
+    promise.then(() => {
+      const usersQuery = `SELECT id as idUser, username, photo, name FROM USERS WHERE id IN (SELECT idUser FROM users_channels WHERE idChannel=${req.params.channel})`;
+      connection.query(usersQuery, async (err, rows) => {
+        if (err) throw err;
+
+        for (let item of rows) {
+          if (item.photo) {
+            const image = await fun.resizeImage(item.photo, 60, 60);
+            item.photo = fun.bufferToBase64(image);
+          }
+          item.isActive = activeUsers.has(item.username);
+        }
+
+        result.users = rows;
+        res.json(result)
+      })
+    })
+  })
+
   socket.on('message-from-user', (message) => {
     console.log('message from user', message);
 
-    message.idUser = "2";
-
-    socket.broadcast.emit('message-from-server', message);
+    const query = `INSERT INTO messages VALUES(NULL, ${message.idSender}, ${message.idChannel}, '${message.message}', current_timestamp())`;
+    connection.query(query, function (err, result) {
+      if (err) throw err;
+      console.log('saved')
+      // socket.broadcast.emit('message-from-server', message);
+      message.idSender = 41;
+      socket.emit('message-from-server', message);
+    })
   })
 
   // setInterval(() => {
@@ -104,8 +143,8 @@ io.on("connection", function (socket) {
   // }, 2000)
 
   socket.on("new-user", function (data) {
-    socket.userId = data;
-    activeUsers.push(data);
+    socket.username = data;
+    activeUsers.add(data);
 
     console.log(activeUsers)
   });
